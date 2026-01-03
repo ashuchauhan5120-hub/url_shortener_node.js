2ab9b686b2b6d17c01ecb358958f2997e7288c68
ashuchauhan5120-hub <ashuchauhan5120@gmail.com>
2026-01-03 14:35:56 +0530

    second commit

--- controllers/shortenController.js
+++ controllers/shortenController.js
@@ -1,44 +1,35 @@
 const { generateShortCode } = require("../utils/generateShortCode");
-// const { getAllUrls, createUrl } = require("../utils/storeUrls");
-const {getUrlByShortcode, creatShortenUrl} = require("../db/crudOperations")
+const { getUrlByShortcode, creatShortenUrl } = require("../db/crudOperations");
 
 function shortenUrlForm(req, res) {
-    res.render("shortenForm");
+  res.render("shortenForm");
 }
 
 async function getShortenUrl(req, res) {
-    const { longUrl } = req.body;
+  const { longUrl } = req.body;
 
-    try {
-        if (!longUrl) {
-            return res.status(404).send("Enter a url");
-        }
-
-        // const urls = getAllUrls();
-        let shortcode;
-        let exists;
-
-        do {
-            shortcode = generateShortCode();
-            exists = await getUrlByShortcode(shortcode)
-            console.log("exists: ", exists)
-        } while (exists);
-
-        // urls[shortcode] = {
-        //     url: longUrl,
-        //     clicks: 0,
-        // };
-         await creatShortenUrl(shortcode, longUrl);
-         console.log("new shorten url:", creatShortenUrl)
-
-        res.redirect(`/result/${shortcode}`)
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server error")
+  try {
+    if (!longUrl) {
+      return res.status(404).send("Enter a url");
     }
+
+    let shortcode;
+    let exists;
+
+    do {
+      shortcode = generateShortCode();
+      exists = await getUrlByShortcode(shortcode);
+      console.log("exists: ", exists);
+    } while (exists);
+    await creatShortenUrl(shortcode, longUrl);
+    res.redirect(`/result/${shortcode}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server error");
+  }
 }
 
 module.exports = {
-    shortenUrlForm,
-    getShortenUrl,
+  shortenUrlForm,
+  getShortenUrl,
 };
